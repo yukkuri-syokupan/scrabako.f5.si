251ddfd50262fc386211ea378019351b1b1df764
yukkuri-syokupan <hukapan0718@gmail.com>
2026-06-28 12:39:28 +0900

    Add files via upload

---
+++ badge-config.js
@@ -0,0 +1,24 @@
+// badge-config.js
+// 🚨 ユーザー名はすべて「小文字」で登録してください
+
+export const BADGE_CONFIG = {
+    // 👑 運営・モデレーター用のカスタム画像
+    blue: {
+        "yukkuri_syokupan": "https://yt-syokupan.f5.si/scratch/images/blue-batch.png",
+        "scratch": "https://example.com"
+    },
+
+    // ✅ 認証済みアカウント用のカスタム画像
+    gold: {
+        "yukkuri_syokupan": "https://yt-syokupan.f5.si/scratch/images/gold-batch.png"
+    },
+
+    gray: {
+        "yukkuri_syokupan": "https://yt-syokupan.f5.si/scratch/images/gray-batch.png",
+        "scratch": "https://example.com"
+    },
+    
+    admin: {
+        "yukkuri_syokupan": "https://yt-syokupan.f5.si/scratch/images/admin-text.png"
+    }
+};
